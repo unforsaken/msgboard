b911ac130658d28ebcce62ac56c5868581845831
unforsaken <unholy_guns@hotmail.com>
2016-09-04 15:54:24 +0800

    add post new action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,25 @@ class PostsController <ApplicationController
 	def show
 		@post = Post.find(params[:id])
 	end
+	def new
+		@post = Post.new
+	end
+	def create
+		post = Post.new(post_params)
+
+		if post.save!
+			#redirect_to new_post_path
+    	redirect_to posts_path
+    	#render :index
+  	else
+    	render :new
+  end
+	end
+
+	private
+
+	def post_params
+		#binding.pry
+		params.require(:post).permit(:title, :content)
+	end
 end

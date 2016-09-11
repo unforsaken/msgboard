51153946f5c96fb158be6ff78b7e7293c72c34bc
unforsaken <unholy_guns@hotmail.com>
2016-09-11 14:03:44 +0800

    changed home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
 resources :posts do
   resources :comments
 end
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+root 'posts#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
